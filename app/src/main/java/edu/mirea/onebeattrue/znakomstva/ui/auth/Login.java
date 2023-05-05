995128db5fa0f0acc8898d11f65abb331240a5b9
Vitaly Sviridov <1beattrue@gmail.com>
2023-05-05 17:03:46 +0300

    adding auth beginning

---
+++ app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/auth/Login.java
@@ -0,0 +1,16 @@
+package edu.mirea.onebeattrue.znakomstva.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.mirea.onebeattrue.znakomstva.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
