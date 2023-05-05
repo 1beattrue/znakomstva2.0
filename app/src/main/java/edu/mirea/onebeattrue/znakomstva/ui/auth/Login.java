ac0aa1050ded042abf48b7c5c3d72fbfbd538132
Vitaly Sviridov <1beattrue@gmail.com>
2023-05-05 18:12:46 +0300

    complete register activity

--- app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/auth/Login.java
+++ app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/auth/Login.java
@@ -3,14 +3,21 @@ package edu.mirea.onebeattrue.znakomstva.ui.auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import edu.mirea.onebeattrue.znakomstva.R;
+import edu.mirea.onebeattrue.znakomstva.databinding.ActivityLoginBinding;
+import edu.mirea.onebeattrue.znakomstva.databinding.ActivityRegisterBinding;
 
 public class Login extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
     }
 }
--- app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/auth/Register.java
+++ app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/auth/Register.java
@@ -1,16 +1,82 @@
 package edu.mirea.onebeattrue.znakomstva.ui.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 import edu.mirea.onebeattrue.znakomstva.R;
+import edu.mirea.onebeattrue.znakomstva.databinding.ActivityRegisterBinding;
+import edu.mirea.onebeattrue.znakomstva.databinding.FragmentAccountBinding;
 
 public class Register extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.LoginNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(binding.email.getText());
+                password = String.valueOf(binding.password.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                binding.progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
