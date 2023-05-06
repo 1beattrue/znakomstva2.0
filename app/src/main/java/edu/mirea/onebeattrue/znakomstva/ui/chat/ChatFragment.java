77eb28cd4eb3a39d4a599ce07a6a8c894e078c86
Vitaly Sviridov <1beattrue@gmail.com>
2023-05-06 18:26:02 +0300

    include chat db

--- app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/chat/ChatFragment.java
+++ app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/chat/ChatFragment.java
@@ -1,20 +1,26 @@
 package edu.mirea.onebeattrue.znakomstva.ui.chat;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import edu.mirea.onebeattrue.znakomstva.databinding.FragmentChatBinding;
 
 public class ChatFragment extends Fragment {
 
     private FragmentChatBinding binding;
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://znakomstva3030-default-rtdb.europe-west1.firebasedatabase.app/");
+    DatabaseReference myRef = database.getReference("message");
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +30,29 @@ public class ChatFragment extends Fragment {
         binding = FragmentChatBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        binding.sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Обработка нажатия на кнопку
+                String message = binding.messageEditText.getText().toString();
+                sendMessage(message);
+            }
+        });
+
         return root;
+    }
+
+    private void sendMessage(String message) {
+        // Проверка, что сообщение не пустое
+        if (!TextUtils.isEmpty(message)) {
+            // Отправка сообщения в Firebase
+            String messageId = myRef.push().getKey();
+            ChatMessage newMessage = new ChatMessage(message, FirebaseAuth.getInstance().getCurrentUser().getUid());
+            myRef.child(messageId).setValue(newMessage);
+
+            // Очистка поля ввода сообщения
+            binding.messageEditText.setText("");
+        }
     }
 
     @Override
---
+++ app/src/main/java/edu/mirea/onebeattrue/znakomstva/ui/chat/ChatMessage.java
@@ -0,0 +1,45 @@
+package edu.mirea.onebeattrue.znakomstva.ui.chat;
+
+import java.util.Date;
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        // Set the current time when the message is created
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage() {
+        // Needed for Firebase
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
